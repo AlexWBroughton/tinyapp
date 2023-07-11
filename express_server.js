6e53087b6bf3848d79ba9d8a3c53f6cd4d4a3cd5
AlexWBroughton <ab.alex.broughton@gmail.com>
2023-07-11 01:39:39 +0000

    random URL short generator

--- express_server.js
+++ express_server.js
@@ -2,7 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
+function generateRandomString() {
+
+  const alphaString = (len, chars='abcdefghijklmnopqrstuvwxyz0123456789') => [...Array(len)].map(() => chars.charAt(Math.floor(Math.random() * chars.length))).join('');
+  
+
+};
+
+
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -10,9 +20,18 @@ const urlDatabase = {
 };
 
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
